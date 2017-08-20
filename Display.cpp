91cf83db2fef7f3497830c493c398c2b7cc73c0e
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2017-08-20 18:23:07 -0400

    starting circle

--- Display.cpp
+++ Display.cpp
@@ -10,35 +10,34 @@ Display::~Display() {}
 
 void Display::initializeGL()
 {
-	glClearColor(1, 1, 1, 1);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_LIGHTING);
-	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-	glEnable(GL_COLOR_MATERIAL);
+	stdInitializeGL();
 }
 
 void Display::paintGL()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(-0.5, -0.5, 0);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(0.5, -0.5, 0);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(0.0, 0.5, 0);
+	float x1, y1, x2, y2;
+	float angle;
+	double radius = 0.1;
+
+	x1 = 0.5, y1 = 0.5;
+	glColor3f(0, 0, 0);
+
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x1, y1);
+
+	for (angle = 1.0f; angle<361.0f; angle += 0.2)
+	{
+		x2 = x1 + sin(angle)*radius;
+		y2 = y1 + cos(angle)*radius;
+		glVertex2f(x2, y2);
+	}
+
 	glEnd();
 }
 
 void Display::resizeGL(int w, int h)
 {
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45, (float)w / h, 0.01, 100.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+	stdResizeGL(w, h);
 }
--- GlUtil.h
+++ GlUtil.h
@@ -9,4 +9,56 @@
 #include <GL/glu.h>
 #endif
 
+struct Vec2f
+{
+	Vec2f(float x, float y) : x(x), y(y) {};
+
+	float x;
+
+	float y;
+};
+
+struct Vec3f
+{
+	Vec3f(float x, float y, float z) : x(x), y(y), z(z) {};
+
+	float x;
+
+	float y;
+
+	float z;
+};
+
+struct Color
+{
+	Color(float r, float g, float b) : r(r), g(g), b(b) {};
+
+	float r;
+
+	float g;
+
+	float b;
+};
+
+inline void stdInitializeGL(Color color = Color(1.0f, 1.0f, 1.0f))
+{
+	glClearColor(color.r, color.g, color.b, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glEnable(GL_COLOR_MATERIAL);
+};
+
+inline void stdResizeGL(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45, (float)w / h, 0.01, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+};
+
 #endif // __GL_UTIL_H__
