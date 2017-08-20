910373b0b3ee7656779bbf19998c2240bc93f161
Tucker Strausbaugh <strausbaugh.tucker@gmail.com>
2017-08-19 21:35:43 -0400

    working on mac

--- Display.h
+++ Display.h
@@ -1,6 +1,12 @@
 #include <QOpenGlWidget>
-#include <gl/GLU.h>
-#include <gl/GL.h>
+
+#ifdef(__APPLE__)
+#include <OpenGL/glu.h>
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
 
 class Display : public QOpenGLWidget
 {
